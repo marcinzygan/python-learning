de81de0e81430e3c3da6bbec0b3af18cec8697d4
marcinzygan <soul@o2.pl>
2025-01-02 18:09:23 +0100

    added lessons

---
+++ 03-section/strings.py
@@ -0,0 +1,10 @@
+print("Today is a good day to learn python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+
+greeting = "Hello"
+name = input("Please provide your name ")
+
+print(greeting + " " + name)
